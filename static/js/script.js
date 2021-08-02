3c5436fdf5a61d77804a682490a1258c943f96dc
code with AlaaZuhd <alaazuhd17@gmail.com>
2021-08-02 21:11:29 +0300

    add icons for prev and nexts img

--- static/js/script.js
+++ static/js/script.js
@@ -5,6 +5,13 @@ const bgModal = document.getElementById("bg-modal")
 const numberOfSizesInput = document.getElementById("number-of-sizes")
 let numberOfSizes = 1
 
+let number_of_images
+let images_url_list = []
+let images_list
+let currentImage
+let imageNumberMessage
+let currentImageNumber
+
 function addProduct() {
     bgModal1.style.display = "block"
 }
@@ -47,19 +54,20 @@ function createNewInput(addAnotherSizeBtn) {
         elements_[i].value = tempInputs[j]
 }
 
+function getData(item) {
+    number_of_images = item.value
+    images_url_list = []
+    images_list = item.parentNode.parentNode.getElementsByTagName('input')
+    currentImage = item.parentNode.parentNode.getElementsByTagName('img')
+    imageNumberMessage = item.parentNode.parentNode.getElementsByTagName('span')[0]
+    currentImageNumber = images_list[0].value
+}
+
 function nextImage(item){
-    let number_of_images = item.value
-    let images_url_list = []
-    let images_list = item.parentNode.getElementsByTagName('input')
-    let currentImage = item.parentNode.getElementsByTagName('img')
-    let imageNumberMessage = item.parentNode.getElementsByTagName('span')[0]
-    let currentImageNumber = images_list[0].value
+    getData(item)
     for (let i=1; i<=number_of_images; i++){
         images_url_list.push(images_list[i].value)
-        console.log(images_url_list[i-1])
     }
-    console.log(number_of_images)
-    console.log(currentImageNumber)
     if(number_of_images > 0){
         if(parseInt(currentImageNumber) === parseInt(number_of_images)) { // display the first image
             currentImage[0].src = images_url_list[0]
@@ -67,6 +75,23 @@ function nextImage(item){
         }else {
                 currentImage[0].src = images_url_list[parseInt(currentImageNumber)]
                 images_list[0].value = parseInt(currentImageNumber) + 1
+        }
+    }
+    imageNumberMessage.textContent = "Image " + (images_list[0].value).toString() + " of " + (number_of_images).toString()
+}
+
+function prevImage(item) {
+    getData(item)
+    for (let i=1; i<=number_of_images; i++){
+        images_url_list.push(images_list[i].value)
+    }
+    if(number_of_images > 0){
+        if(parseInt(currentImageNumber) === 1) { // display the first image
+            currentImage[0].src = images_url_list[parseInt(number_of_images) -1]
+            images_list[0].value = parseInt(number_of_images) -1
+        }else {
+                currentImage[0].src = images_url_list[parseInt(currentImageNumber)-2]
+                images_list[0].value = parseInt(currentImageNumber) - 1
         }
     }
     imageNumberMessage.textContent = "Image " + (images_list[0].value).toString() + " of " + (number_of_images).toString()
